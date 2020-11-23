50b96ae66ced278762e495eb44f507f7f84978a5
Luke <uklukehemmings@gmail.com>
2020-11-23 16:21:15 +0000

    Added header.php

--- KF7013-2021/content/php/header.php
+++ KF7013-2021/content/php/header.php
@@ -1,16 +1,14 @@
 <?php
 
 
-function footer ($headerText) {
+function makeHeader ($headerText) {
 	$headContent = <<<HEAD
-		<head>
 		<title>$headerText</title>
 		<meta charset="utf-8">
 		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="css/stylesheet.css">
+		<link rel="stylesheet" href="./assets/stylesheets/main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="Keywords" content="travel, fun, hotels, flights, trip, travelling, photographer, beach, style, sunset, holiday, lifestyle, life, traveltheworld, beauty, mountains, sea, tourism, traveler, traveller, architecture">
-		</head>
 	HEAD;
 	return $headContent;
 }
---
+++ KF7013-2021/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include './content/php/header.php'; 
+	echo makeHeader("Matsumoto Tourism - Home");
+	?>
+</head>
+<body>
+	<p>Page with header</p>
+		
+</body>
+</html>
