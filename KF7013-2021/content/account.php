9ddcf9371e8aa8c6a4c8fb229c4ca633b5a2459a
Luke <uklukehemmings@gmail.com>
2020-12-01 23:15:30 +0000

    added account.php

---
+++ KF7013-2021/content/account.php
@@ -0,0 +1,44 @@
+<?php
+/*
+	session_start();
+	
+	if (isset($_SESSION['username'])) {
+		//echo 'Session set';
+	} else {
+		//header("Location: ./login.php");
+	}
+	*/
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="../assets/stylesheets/main.css">
+	<?php 
+	include './php/head.php';
+	include './php/navbar.php';
+	echo makeHead("Matsumoto Tourism - Home");
+	?>
+</head>
+<body class="<?php echo $themeType; ?>">
+	<div id="container"> <!-- This container is necessary to make sure the footer stays where it belongs -->
+		<nav id="topnav">
+			<?php 
+			makeNavBar($themeType); 
+			?>
+		</nav>
+		<main id="content"> <!-- Beginning of page content -->
+			<p>Account page</p>
+			<?php
+				echo 'Username: ' . $_SESSION['userName'];
+			?>
+		</main>
+
+		<footer id="footer"> <!-- Beginning of footer -->
+
+		</footer>
+	</div>
+</body>
+</html>
+
