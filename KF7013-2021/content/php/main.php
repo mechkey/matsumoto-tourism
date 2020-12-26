dd4d4a10ca5ba4a815e2afb1a9dfbd5ef1c5a3b3
Luke <uklukehemmings@gmail.com>
2020-12-26 22:12:36 +0000

    refactored navbar

--- KF7013-2021/content/php/main.php
+++ KF7013-2021/content/php/main.php
@@ -13,16 +13,6 @@
 
 	if ($conn === false) {
 		echo "<p>conn failed:" . mysqli_connect_error() . " </p>\n";
-	}
-
-	//Init dark mode vars
-	$themeType = '';
-	//$_SESSION['themeType'] = '';
-	$changeTheme = '';
-	if (((isset($_REQUEST['theme'])) && ($_REQUEST['theme'] == 'dark'))) {
-		$_SESSION['themeType'] = 'dark';
-	} else if ((isset($_REQUEST['theme'])) && ($_REQUEST['theme'] == 'light')) {
-		$_SESSION['themeType'] = 'light';
 	} 
 
 	//This function prints out the content of the activities table.
@@ -173,64 +163,7 @@
 		}
 	}
 
-	function classID() {
-		if (isset($_SESSION['themeType'])) {
-			echo $_SESSION['themeType'];
-		} else {
-			echo $_SESSION['themeType'] = 'light';
-		}
-	}
 
-	function contentAsArray($rel) { 
-		global $debug;
-		$tmpPageArray = [];
-		foreach (glob($rel . '*.php') as $filename)
-		{
-			if ($debug) { echo $filename; }
-			//if it isn't a page to do with login/out
-			if( 
-				($filename != ($rel . 'login.php')) && 
-				($filename != ($rel . 'logout.php')) && 
-				($filename != ($rel . 'account.php')) &&
-				($filename != ($rel . 'register.php')) 
-				)  
-			{
-				//then push it to the array
-				array_push($tmpPageArray, $filename);   
-			}
-		}
-		//if it is a login/registration page AND the user is not logged in, add it to the end of the array
-		if (!(isset($_SESSION['username']))) { // if not loggged in
-			//echo $curpagepath;	
-			array_push( $tmpPageArray, $rel . 'register.php' );
-			array_push( $tmpPageArray, $rel . 'login.php' );
-		//else if it a must be logged in page, only show it if logged in, and add it to the end of the array
-		} else { // if logged in
-			array_push( $tmpPageArray, $rel . 'account.php' );
-			array_push( $tmpPageArray, $rel . 'logout.php' );
-		}
-		return $tmpPageArray;
-	} 
-
-	function fallbacktheme() {//Fall back Dark mode / light form mode button
-		//$themeType = getTT();
-		echo '<li>';
-		$changeTheme = ($themeType == 'light') ? 'dark' : 'light';
-		$ctstring = '<a id="ctheme" href="?theme=' . $changeTheme;
-		if ($themeType == 'light') {
-			$ctstring .= '">Dark Mode';
-		} else {
-			$ctstring .= '">Light Mode';
-		}
-		echo $ctstring . '</a></li>';
-
-		echo '<li><form id="theme" method="post" action="./index.php"><input type="submit" name="theme" value="dark"/> </form></li>';
-	}
-
-	function getpath() {
-		$path = $_SERVER['REQUEST_URI'];
-		return $path;
-	}
 
 	//
 	/*
@@ -353,16 +286,7 @@
 	}
 
 	//The website logo light mode and dark mode selector.
-	function showLogo ($path) {
-		$logosrc = ''; 	
-		if (isset($_SESSION['themeType']) && $_SESSION['themeType'] == 'dark') { 		
-			$logosrc = '/KF7013-2021/assets/images/logogray.png'; 	
-		} else { 
-			$logosrc = '/KF7013-2021/assets/images/logo.png';
-		}
-		
-		echo '<li id="navlogo"><a href="/KF7013-2021/index.php"><img id="logo" src="' . $logosrc . '" alt="Visit Matsumoto Logo" height="30"/></a></li>';
-	}
+
 
 	//Shows the account, first and last names for the logged in user.
 	function showDetails () {
--- KF7013-2021/content/php/navbar.php
+++ KF7013-2021/content/php/navbar.php
@@ -1,14 +1,19 @@
 <?php
-	//include 'path.php';
-
 	include 'main.php';
-	//Logo includes main.php
 
-	//function makeNavBar ($themeType) {
+	//Init dark mode vars
+	$themeType = '';
+	//$_SESSION['themeType'] = '';
+	$changeTheme = '';
+	if (((isset($_REQUEST['theme'])) && ($_REQUEST['theme'] == 'dark'))) {
+		$_SESSION['themeType'] = 'dark';
+	} else if ((isset($_REQUEST['theme'])) && ($_REQUEST['theme'] == 'light')) {
+		$_SESSION['themeType'] = 'light';
+	}
+
 	function makeNavBar () {
 
 		//Navigation bar generation
-
 		$debug = false;
 
 		echo '<ul>';
@@ -92,5 +97,80 @@
 		*/
 		echo"</ul>";
 		//return $navContent;
+	}
+
+	// ***
+	//Supporting functions in alphabetical order
+	// ***
+
+	function classID() {
+		if (isset($_SESSION['themeType'])) {
+			echo $_SESSION['themeType'];
+		} else {
+			echo $_SESSION['themeType'] = 'light';
 		}
+	}
+
+	function contentAsArray($rel) { 
+		global $debug;
+		$tmpPageArray = [];
+		foreach (glob($rel . '*.php') as $filename)
+		{
+			if ($debug) { echo $filename; }
+			//if it isn't a page to do with login/out
+			if( 
+				($filename != ($rel . 'login.php')) && 
+				($filename != ($rel . 'logout.php')) && 
+				($filename != ($rel . 'account.php')) &&
+				($filename != ($rel . 'register.php')) 
+				)  
+			{
+				//then push it to the array
+				array_push($tmpPageArray, $filename);   
+			}
+		}
+		//if it is a login/registration page AND the user is not logged in, add it to the end of the array
+		if (!(isset($_SESSION['username']))) { // if not loggged in
+			//echo $curpagepath;	
+			array_push( $tmpPageArray, $rel . 'register.php' );
+			array_push( $tmpPageArray, $rel . 'login.php' );
+		//else if it a must be logged in page, only show it if logged in, and add it to the end of the array
+		} else { // if logged in
+			array_push( $tmpPageArray, $rel . 'account.php' );
+			array_push( $tmpPageArray, $rel . 'logout.php' );
+		}
+		return $tmpPageArray;
+	} 
+
+
+		function fallbacktheme() {//Fall back Dark mode / light form mode button
+		//$themeType = getTT();
+		echo '<li>';
+		$changeTheme = ($themeType == 'light') ? 'dark' : 'light';
+		$ctstring = '<a id="ctheme" href="?theme=' . $changeTheme;
+		if ($themeType == 'light') {
+			$ctstring .= '">Dark Mode';
+		} else {
+			$ctstring .= '">Light Mode';
+		}
+		echo $ctstring . '</a></li>';
+
+		echo '<li><form id="theme" method="post" action="./index.php"><input type="submit" name="theme" value="dark"/> </form></li>';
+	}
+
+	function getpath() {
+		$path = $_SERVER['REQUEST_URI'];
+		return $path;
+	}
+
+	function showLogo ($path) {
+		$logosrc = ''; 	
+		if (isset($_SESSION['themeType']) && $_SESSION['themeType'] == 'dark') { 		
+			$logosrc = '/KF7013-2021/assets/images/logogray.png'; 	
+		} else { 
+			$logosrc = '/KF7013-2021/assets/images/logo.png';
+		}
+		
+		echo '<li id="navlogo"><a href="/KF7013-2021/index.php"><img id="logo" src="' . $logosrc . '" alt="Visit Matsumoto Logo" height="30"/></a></li>';
+	}
 ?>
